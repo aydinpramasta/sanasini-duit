e0e32b936443f9ee5e36390b0242d4286c248188
Aydin Pramasta <75928044+aydinpramasta@users.noreply.github.com>
2025-10-27 00:40:28 +0700

    Update application title in layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
-  title: 'SanaSini Duit Lite',
+  title: 'SanaSini Duit',
   description: 'Pencatatan keuangan otomatis yang praktis dan real-time.',
 };
 
